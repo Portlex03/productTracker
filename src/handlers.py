27e53744dcb8fe7726493197703b836ca01b7568
Portlex <portnyagin924@gmail.com>
2024-12-07 17:09:50 +0600

    feat: создание новых файлов

---
+++ src/handlers.py
@@ -0,0 +1,70 @@
+from os import getenv
+
+from aiogram import Router, F
+from aiogram.filters import Command
+from aiogram.types import Message, CallbackQuery
+from dotenv import load_dotenv
+
+from backend_api_connector import get_products
+from db_api_connector import StoresDBConnector, ProductsDBConnector
+from keyboards import create_product_keyboard
+
+load_dotenv()
+
+PRODUCTS_API_TOKEN: str = getenv("PRODUCTS_API_TOKEN")
+
+router = Router()
+
+stores_db_connector = StoresDBConnector()
+products_db_connector = ProductsDBConnector()
+
+
+@router.message(Command("start"))
+async def command_start_handler(message: Message) -> None:
+    store = stores_db_connector.get_store_data_from_chat_id(message.chat.id)
+    await message.answer(
+        "Бот подключился успешно!\nНаименование магазина: {}".format(
+            store["store_name"]
+        )
+    )
+
+
+@router.message(Command("products"))
+async def get_products_handler(message: Message) -> None:
+    store: dict = stores_db_connector.get_store_data_from_chat_id(message.chat.id)
+    products: list[dict] = get_products(
+        token=PRODUCTS_API_TOKEN,
+        store_id=store["store_id"],
+        count_items=store["store_count_items"],
+    )
+
+    for product in products:
+        product_name = product["beautifulName"]
+        product_code = product["code"]
+        keyboard = create_product_keyboard(product_code)
+
+        await message.answer(text=f"Товар: {product_name}", reply_markup=keyboard)
+
+
+@router.callback_query(F.data.startswith("on_shelf"))
+async def handle_on_shelf(call: CallbackQuery) -> None:
+    product_id = call.data.split(":")[-1]
+    store = stores_db_connector.get_store_data_from_chat_id(call.message.chat.id)
+    store_id = store["store_id"]
+
+    products_db_connector.insert_shelf_status(
+        prod_id=product_id, prod_store_id=store_id, prod_avail=True
+    )
+    await call.answer("Информация о товаре записана: 'Есть на полке'.")
+
+
+@router.callback_query(F.data.startswith("off_shelf"))
+async def handle_off_shelf(call: CallbackQuery) -> None:
+    product_id = call.data.split(":")[-1]
+    store = stores_db_connector.get_store_data_from_chat_id(call.message.chat.id)
+    store_id = store["store_id"]
+
+    products_db_connector.insert_shelf_status(
+        prod_id=product_id, prod_store_id=store_id, prod_avail=False
+    )
+    await call.answer("Информация о товаре записана: 'Нет на полке'.")
---
+++ src/keyboards.py
@@ -0,0 +1,14 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def create_product_keyboard(product_id: int) -> InlineKeyboardMarkup:
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(text="Есть на полке", callback_data=f"on_shelf:{product_id}"),
+                InlineKeyboardButton(text="Нет на полке", callback_data=f"off_shelf:{product_id}"),
+                InlineKeyboardButton(text="Добавить фото", callback_data=f"add_photo:{product_id}"),
+            ]
+        ]
+    )
+    return keyboard

1a5a9b80f67fa364f260e28cf862a9ac40c62e9a
Portlex <portnyagin924@gmail.com>
2025-01-05 20:33:33 +0600

    fix: insert_product_func

--- src/db_api_connector.py
+++ src/db_api_connector.py
@@ -2,7 +2,7 @@ from supabase import create_client, Client
 
 STORES_TABLE_NAME = "ParfumLeaderStores"
 
-PRODUCTS_TABLE_NAME = "ProductsTracking"
+PRODUCTS_TABLE_NAME = "Products"
 
 
 class Connector:
@@ -20,37 +20,31 @@ class Connector:
 
 class StoresDBConnector(Connector):
     def get_store_data_from_chat_id(self, chat_id: int) -> dict:
-        response = (
-            self.supabase
-            .table(STORES_TABLE_NAME)
+        responce = (
+            self.supabase.table(STORES_TABLE_NAME)
             .select("*")
             .eq("chat", chat_id)
             .execute()
         )
-        response_data: list[dict] = response.data
-        store: dict = response_data[-1]
+        responce_data: list[dict] = responce.data
+        store: dict = responce_data[-1]
         return store
 
 
 class ProductsDBConnector(Connector):
     table_name: str = PRODUCTS_TABLE_NAME
 
-    prod_id: str = "prod_id"
-    prod_name: str = "prod_name"
-    prod_avail: str = "prod_avail"
-    prod_store_id: str = "prod_store_id"
-
-    def insert_shelf_status(
-        self, prod_id: str, prod_store_id: int, prod_avail: bool, prod_name: str
+    def insert_product(
+        self, prod_id: str, prod_name: str, prod_avail: bool, prod_store_code: str
     ) -> None:
-        record = {
-            self.prod_id: prod_id,
-            self.prod_store_id: prod_store_id,
-            self.prod_avail: prod_avail,
-            self.prod_name: prod_name,
-        }
-
-        self.supabase.table(self.table_name).insert(record).execute()
+        self.supabase.table(self.table_name).insert(
+            {
+                "prod_id": prod_id,
+                "prod_name": prod_name,
+                "prod_avail": prod_avail,
+                "prod_store_code": prod_store_code,
+            }
+        ).execute()
 
 
 stores_db_connector = StoresDBConnector()

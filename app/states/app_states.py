f09de846b1c338c579185f3ccc7a7a54f404c7a9
Server <admin@student-telegram.shkinev.me>
2025-02-03 07:19:19 +0000

    fix: change states location and add appstate

--- app/states/app_states.py
+++ app/states/app_states.py
@@ -1,6 +1,11 @@
 from aiogram.fsm.state import State, StatesGroup
 
 
+class AppState(StatesGroup):
+    chat_store = State()
+    last_prod_list = State()
+
+
 class AddPhotoState(StatesGroup):
     product_id = State()
     upload_photo = State()

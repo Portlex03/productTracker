281c7246bcce74945888ca0320c6f0d06c6525e9
Server <admin@student-telegram.shkinev.me>
2025-02-03 07:18:21 +0000

    fix: delete photo button

--- app/keyboards/prod_keyboard.py
+++ app/keyboards/prod_keyboard.py
@@ -14,20 +14,7 @@ def products_keyboard() -> InlineKeyboardMarkup:
                 InlineKeyboardButton(
                     text="Нет на полке", callback_data=f"prod.off_shelf.{prod_id}"
                 ),
-                InlineKeyboardButton(
-                    text="Добавить фото", callback_data=f"add_photo.{prod_id}"
-                ),
             ]
         ]
     )
-    return keyboard
-
-
-def updated_products_keyboard(
-    inline_kb: list[list[InlineKeyboardButton]], buttons_data2delete: list[str]
-    ) -> InlineKeyboardMarkup:
-    new_inline_kb = [
-        [button for button in row if button.callback_data not in buttons_data2delete] for row in inline_kb
-    ]
-    keyboard = InlineKeyboardMarkup(inline_keyboard=new_inline_kb)
     return keyboard

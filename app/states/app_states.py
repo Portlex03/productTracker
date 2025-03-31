73bd0ae402aae9e561594caa98e8c578a89feefc
Server <admin@student-telegram.shkinev.me>
2025-03-31 05:57:39 +0000

    fix: delete AddPhotoState

--- app/states/app_states.py
+++ app/states/app_states.py
@@ -4,8 +4,3 @@ from aiogram.fsm.state import State, StatesGroup
 class AppState(StatesGroup):
     store_data = State()
     last_prod_list = State()
-
-
-class AddPhotoState(StatesGroup):
-    product_id = State()
-    upload_photo = State()

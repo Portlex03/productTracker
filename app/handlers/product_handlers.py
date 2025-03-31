af4f25f63befe31a10b7fd250a639f2e55882cef
Server <admin@student-telegram.shkinev.me>
2025-03-31 05:50:18 +0000

    fix: change handlers

--- app/handlers/product_handlers.py
+++ app/handlers/product_handlers.py
@@ -7,7 +7,7 @@ from aiogram.types import CallbackQuery, Message
 
 from ..api.perfume_backend import get_products_request
 from ..config import app_settings
-from ..database import stores_table, products_table
+from ..database import products_available_table, products_table
 from ..keyboards.prod_keyboard import products_keyboard
 
 from ..states.app_states import AppState
@@ -15,16 +15,18 @@ from ..states.app_states import AppState
 router = Router()
 
 
-@router.message(Command("p"))
+@router.message(Command("_p"))
 async def send_prod_list_to_chat_handler(message: Message, state: FSMContext) -> None:
-    await state.set_state(AppState.chat_store)
-    chat_store: dict = await state.get_value("chat_store")
+    await state.set_state(AppState.store_data)
+    store_data: dict = await state.get_value("store_data")
 
     products: list[dict] = get_products_request(
         token=app_settings.perfume_backend_api_token,
-        store_id=chat_store["code"],
-        count_items=chat_store["prod_count"],
+        store_id=store_data["code"],
+        count_items=store_data["prod_count"],
     )
+
+    products_table.insert_products(products)
 
     last_prod_list = []
     for product in products:
@@ -38,7 +40,7 @@ async def send_prod_list_to_chat_handler(message: Message, state: FSMContext) ->
     await state.update_data(last_prod_list=last_prod_list)
 
 
-@router.message(Command("d"))
+@router.message(Command("_d"))
 async def delete_last_prod_list_handler(
     message: Message, state: FSMContext, bot: Bot
 ) -> None:
@@ -52,27 +54,26 @@ async def delete_last_prod_list_handler(
     await bot.delete_messages(chat_id=message.chat.id, message_ids=last_prod_list)
 
 
-@router.callback_query(F.data.startswith("prod"))
-async def on_shelf_handler(call: CallbackQuery, bot: Bot) -> None:
-    _, prod_avail, prod_id = call.data.split(".")
-    prod_avail = True if prod_avail == "on_shelf" else False
+@router.callback_query(F.data.startswith("kb.prod_avail"))
+async def on_shelf_handler(call: CallbackQuery, state: FSMContext, bot: Bot) -> None:
+    prod_name = call.message.text.split(": ")[-1]
+    prod_id = products_table.get_product_id_by_name(prod_name)
+    prod_avail = True if call.data.split(".")[-1] == "true" else False
 
-    store = stores_table.get_store_data_from_chat_id(call.message.chat.id)
+    await state.set_state(AppState.store_data)
+    store_data: dict = await state.get_value("store_data")
+
     product = {
         "prod_id": prod_id,
-        "prod_name": call.message.text.split(": ")[-1],
         "prod_avail": prod_avail,
-        "prod_store_code": store["code"],
-        "prod_employee_name": call.from_user.full_name,
+        "prod_store_id": store_data["id"],
+        "prod_employee_id": call.from_user.id,
     }
-    products_table.insert_product(product)
+    products_available_table.insert_product_avail(product)
 
     prod_status = "Есть на полке" if prod_avail else "Нет на полке"
-    await call.answer(
-        "Товар '{}' отмечен успешно.\nСтатус: {}".format(
-            product["prod_name"], prod_status
-        )
-    )
+    message = "Товар '{}' отмечен успешно.\nСтатус: {}"
+    await call.answer(message.format(prod_name, prod_status))
 
     await bot.delete_message(
         chat_id=call.message.chat.id, message_id=call.message.message_id

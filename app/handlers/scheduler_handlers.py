ba7ca5ed2821eb86eefffa0b08846327db38e556
Server <admin@student-telegram.shkinev.me>
2025-04-06 09:21:32 +0000

    feat: change scheduler logic

--- app/handlers/scheduler_handlers.py
+++ app/handlers/scheduler_handlers.py
@@ -4,12 +4,10 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-# from apscheduler.triggers.cron import CronTrigger
-from apscheduler.triggers.interval import IntervalTrigger
-
+from ..states.app_states import AppState
 from .product_handlers import (
-    send_prod_list_to_chat_handler,
     delete_last_prod_list_handler,
+    send_prod_list_to_chat_handler,
 )
 
 router = Router()
@@ -21,14 +19,33 @@ scheduler = AsyncIOScheduler()
 async def run_prod_scheduler_handler(
     message: Message, state: FSMContext, bot: Bot
 ) -> None:
-    trigger = IntervalTrigger(minutes=20)
+    await state.set_state(AppState.store_data)
+    store_data: dict = await state.get_value("store_data")
 
-    scheduler.add_job(
-        delete_last_prod_list_handler, trigger, args=(message, state, bot)
-    )
-    scheduler.add_job(
-        send_prod_list_to_chat_handler, trigger, args=(message, state)
-    )
+    check_start_time = store_data["prod_check_time"]
+    start_hour, start_minute, _ = map(int, check_start_time.split(":"))
+
+    daily_checks_count = store_data["daily_checks_count"]
+    daily_checks_interval = store_data["daily_checks_interval"]
+
+    for i in range(daily_checks_count):
+        call_hour = start_hour + i * daily_checks_interval
+
+        scheduler.add_job(
+            delete_last_prod_list_handler,
+            "cron",
+            args=(message, state, bot),
+            hour=call_hour,
+            minute=start_minute,
+        )
+
+        scheduler.add_job(
+            send_prod_list_to_chat_handler,
+            "cron",
+            args=(message, state),
+            hour=call_hour,
+            minute=start_minute,
+        )
 
     scheduler.start()
 
